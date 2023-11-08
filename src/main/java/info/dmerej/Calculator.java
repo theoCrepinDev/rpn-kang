1bc759bed1e8f7228edb98db08f335e4b9d1ab98
Youyyoub <74156463+Youyyoub@users.noreply.github.com>
2023-11-08 15:02:25 +0100

    Addition

---
+++ src/main/java/info/dmerej/Calculator.java
@@ -0,0 +1,37 @@
+package info.dmerej;
+
+import java.util.Stack;
+
+public class Calculator {
+    // private static final String operators = "+-/*";
+
+    public int RPNEvaluation(String expression) {
+        String[] tokens = expression.split(" ");
+        Stack<Integer> stack = new Stack<Integer>();
+
+        int result = 0;
+
+        for (String token : tokens) {
+            if (isInteger(token)) {
+                stack.push(Integer.parseInt(token));
+            }
+            else {
+                while (!stack.empty()) {
+                    result += stack.pop();
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public boolean isInteger(String string) {
+        try {
+            Integer.parseInt(string);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
